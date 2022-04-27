8ed2d3f72e4f56c4c0a6cec421742aa2ee4b00a8
camaro.emi@outlook.com <camaro.emi@outlook.com>
2022-04-27 09:43:43 -0500

    Banner en todas las pantallas

--- ParqueProcesoDesarollo_Estacionamiento/PA-08-03 Pantalla Principal Control de Máquinas.Designer.cs
+++ ParqueProcesoDesarollo_Estacionamiento/PA-08-03 Pantalla Principal Control de Máquinas.Designer.cs
@@ -32,13 +32,16 @@
             this.btnComprobarMonto = new MetroFramework.Controls.MetroButton();
             this.btnRegistrarMonto = new MetroFramework.Controls.MetroButton();
             this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // lblRegresar
             // 
             this.lblRegresar.AutoSize = true;
             this.lblRegresar.FontSize = MetroFramework.MetroLabelSize.Tall;
-            this.lblRegresar.Location = new System.Drawing.Point(677, 68);
+            this.lblRegresar.Location = new System.Drawing.Point(2144, 261);
+            this.lblRegresar.Margin = new System.Windows.Forms.Padding(10, 0, 10, 0);
             this.lblRegresar.Name = "lblRegresar";
             this.lblRegresar.Size = new System.Drawing.Size(75, 25);
             this.lblRegresar.TabIndex = 7;
@@ -47,9 +50,10 @@
             // 
             // btnComprobarMonto
             // 
-            this.btnComprobarMonto.Location = new System.Drawing.Point(608, 329);
+            this.btnComprobarMonto.Location = new System.Drawing.Point(1925, 936);
+            this.btnComprobarMonto.Margin = new System.Windows.Forms.Padding(10, 9, 10, 9);
             this.btnComprobarMonto.Name = "btnComprobarMonto";
-            this.btnComprobarMonto.Size = new System.Drawing.Size(144, 44);
+            this.btnComprobarMonto.Size = new System.Drawing.Size(456, 125);
             this.btnComprobarMonto.TabIndex = 6;
             this.btnComprobarMonto.Text = "Comprobar Monto Actual";
             this.btnComprobarMonto.UseSelectable = true;
@@ -57,9 +61,10 @@
             // 
             // btnRegistrarMonto
             // 
-            this.btnRegistrarMonto.Location = new System.Drawing.Point(446, 329);
+            this.btnRegistrarMonto.Location = new System.Drawing.Point(1412, 936);
+            this.btnRegistrarMonto.Margin = new System.Windows.Forms.Padding(10, 9, 10, 9);
             this.btnRegistrarMonto.Name = "btnRegistrarMonto";
-            this.btnRegistrarMonto.Size = new System.Drawing.Size(144, 44);
+            this.btnRegistrarMonto.Size = new System.Drawing.Size(456, 125);
             this.btnRegistrarMonto.TabIndex = 5;
             this.btnRegistrarMonto.Text = "Registrar Monto Inicial";
             this.btnRegistrarMonto.UseSelectable = true;
@@ -69,27 +74,42 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Century Gothic", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(80, 122);
+            this.label1.Location = new System.Drawing.Point(253, 347);
+            this.label1.Margin = new System.Windows.Forms.Padding(10, 0, 10, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(624, 156);
+            this.label1.Size = new System.Drawing.Size(1885, 460);
             this.label1.TabIndex = 4;
             this.label1.Text = "ESTACIONAMIENTO\r\nFUZZY ROAD";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ParqueProcesoDesarollo_Estacionamiento.Properties.Resources.Banner;
+            this.pictureBox1.Location = new System.Drawing.Point(15, 71);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(2428, 177);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 50;
+            this.pictureBox1.TabStop = false;
+            // 
             // frmPantallaPrincipalControl
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(19F, 37F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(775, 408);
+            this.ClientSize = new System.Drawing.Size(2454, 1161);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.lblRegresar);
             this.Controls.Add(this.btnComprobarMonto);
             this.Controls.Add(this.btnRegistrarMonto);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(10, 9, 10, 9);
             this.Name = "frmPantallaPrincipalControl";
+            this.Padding = new System.Windows.Forms.Padding(63, 171, 63, 57);
             this.Resizable = false;
             this.Text = "PA-08-03 Pantalla Principal Control de Máquinas";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.frmPantallaPrincipalControl_FormClosed);
             this.Load += new System.EventHandler(this.frmPantallaPrincipalControl_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -101,5 +121,6 @@
         private MetroFramework.Controls.MetroButton btnComprobarMonto;
         private MetroFramework.Controls.MetroButton btnRegistrarMonto;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
